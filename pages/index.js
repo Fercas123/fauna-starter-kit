ff5e57ca7df75faf5eb592a8d5b770c0c9ebb770
Akhil Shankar <akhilshankar@Akhils-MacBook-Air.local>
2023-01-10 20:57:56 +0000

    remove rsvp stuff

--- pages/index.js
+++ pages/index.js
@@ -181,18 +181,14 @@ const Guestbook = ({fallback}) => {
             <AppHead/>
             <header
                 className="border-b-2 flex sticky top-0 mx-[1rem] pt-[1rem] pb-[0.2rem] bg-white items-center justify-center flex-col	">
-                <button
-                    className="border rounded-md pt-[0.25rem] px-[0.75rem] font-bold self-end bg-green-600 hover:bg-green-900 text-white text-sm">RSVP
-                </button>
+                {/*<button*/}
+                {/*    className="border rounded-md pt-[0.25rem] px-[0.75rem] font-bold self-end bg-green-600 hover:bg-green-900 text-white text-sm">RSVP*/}
+                {/*</button>*/}
                 <h1 className="text-4xl font-highlight">Ursula & Luis</h1>
-                <nav>
-                    <a className="text-xs px-[1rem] hover:text-green-700" href="#eventos">Eventos</a>
-                    <a className="text-xs px-[1rem] hover:text-green-700" href="#regalos">Mesa de regalos</a>
-                    <a className="text-xs px-[1rem] hover:text-green-700" href="#rsvp">RSVP</a>
-                </nav>
+                <p className="text-xs px-[1rem] hover:text-green-700">Nuestra Boda</p>
             </header>
             <main className="max-w-4xl mx-auto p-4 m-0 font-sans text-sm">
-                <section className="py-[1.2rem] text-center grid justify-center">
+                <section className="py-[2rem] text-center grid justify-center">
                     <div
                         className="mb-0 max-h-[30vh] max-w-full overflow-hidden container flex items-center justify-center h-screen m-auto bg-fixed bg-center bg-cover custom-img">
                         <img className="my-[2rem] max-w-full" src="/static/ursuyluis.jpg"/>
@@ -201,12 +197,13 @@ const Guestbook = ({fallback}) => {
 
                     <p>Lo tenemos todo. El traje, el lugar, las flores, el mezcal...</p>
                     <p>Pero nos falta lo básico: ¡Contar contigo en este día tan importante para nosotros!</p>
-                    <p>Asi que por favor confirma lo que ya sabemos: ¡vienes seguro!</p>
-                    <p>La ceremonia tendrá lugar el</p>
-                    <h2 className="text-lg font-bold">Sabado 15 de Abril 2023 en Oaxaca, Mexico</h2>
+                    <p>Asi que por favor confirma lo que ya sabemos: ¡Que vienes seguro!</p>
+                    <p className="pt-[3rem]">La ceremonia tendrá lugar el</p>
+                    <h2 className="text-lg font-bold">Sabado 15 de Abril 2023 </h2>
+                        <h2 className="text-lg font-bold">en Oaxaca, Mexico</h2>
                 </section>
                 <hr className="mx-auto my-[1rem] w-28 transition transition-[width] ease-linear duration-1000	"/>
-                <section className="py-[1.2rem] text-center flex flex-wrap justify-center gap-[3rem]" id="eventos">
+                <section className="py-[2rem] text-center flex flex-wrap justify-center gap-[3rem]" id="eventos">
                     <div className="text-gray-600 m-[0.2rem]">
                         <h3 className="font-black text-black text-lg">BODA RELIGIOSA</h3>
                         <p>TEMPLO SAN MATÍAS JALATLACO</p>
@@ -229,34 +226,30 @@ const Guestbook = ({fallback}) => {
                     </div>
                 </section>
                 <hr className="mx-auto my-[1rem] w-28 transition transition-[width] ease-linear duration-1000	"/>
-                <section className="py-[1.2rem] text-center" id="regalos">
+                <section className="py-[2rem] text-center" id="regalos">
                     <h3 className="font-black text-black text-lg">MESA DE REGALOS</h3>
                     <p>El mejor regalo que nos puedes dar es tu presencia,</p>
                     <p className="pb-[1rem]">pero si quieres obsequiarnos algo puedes hacerlo de dos formas:</p>
                     <a className="text-base p-[1rem] hover:text-green-700" href="https://www.amazon.com.mx/wedding/share/ushu_y_luis">Amazon </a>
                     <a className="text-base p-[1rem] hover:text-green-700" href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/50945324">Liverpool </a>
                 </section>
-                <div
-                    className={cn(
-                        'rounded p-6',
-                        'my-4 w-full dark:border-gray-800 bg-green-100',
-                        'dark:bg-green-opaque'
-                    )}
-                >
-                    <h5 className={cn('text-lg md:text-xl font-bold', 'text-gray-900')}>
+                <hr className="mx-auto my-[1rem] w-28 transition transition-[width] ease-linear duration-1000	"/>
+                <section className="py-[2rem] text-center" id="rsvp">
+
+                <h5 className={cn('text-lg md:text-xl font-bold', 'text-gray-900')}>
                         RSVP
                     </h5>
                     <p className="my-1 text-gray-800">
-                        Por favor responde antes del 10 de enero del 2023, esperamos poder celebrar contigo!
+                        Por favor responde antes del 15 de marzo del 2023, esperamos poder celebrar contigo!
                     </p>
-                    <p>FORMULARIO EN CONSTRUCCION</p>
+                    <p>Telefono/Whatsapp: 951 576 9893 / 951 Balan</p>
                     {/*<EntryForm onSubmit={onSubmit}/>*/}
-                </div>
-                <div className="mt-4 space-y-8 px-2">
-                    {entries?.map((entry) => (
-                        <EntryItem key={entry._id} entry={entry}/>
-                    ))}
-                </div>
+                </section>
+                {/*<div className="mt-4 space-y-8 px-2">*/}
+                {/*    {entries?.map((entry) => (*/}
+                {/*        <EntryItem key={entry._id} entry={entry}/>*/}
+                {/*    ))}*/}
+                {/*</div>*/}
             </main>
             <footer
                 className="text-xs border-t-2 flex sticky bottom-0 mx-[1rem] py-[1rem] bg-white items-center justify-center flex-col"> Ursula
