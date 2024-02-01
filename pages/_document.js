82dc57c5ddc7972e68876f2018a758fff42e8b07
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-02-01 22:51:03 +0000

    move template to avoid it rerendering, clean form from state, fixes in list (#9)

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import {styles} from "./index";
+import Link from "next/link";
+import {constants} from "../constants";
+import {format} from "date-fns";
+import localeEs from "date-fns/locale/es/index";
 
 export default function Document() {
     return (
@@ -21,8 +26,19 @@ export default function Document() {
             />
             </Head>
             <body>
+            <header className={styles.header}>
+                <div className={styles.headerLine}>
+                    <Link href={'/'}>
+                        <h1 className={styles.headerNames}>{`${constants.bride} & ${constants.groom}`}</h1>
+                    </Link>
+                    <p className={styles.headerEvent}>Nuestra boda</p>
+                </div>
+            </header>
             <Main />
             <NextScript />
+            <footer className={styles.footer}>
+                {`${constants.bride} & ${constants.groom} | ${format(constants.date, 'MMMM yyyy', {locale: localeEs})}`}
+            </footer>
             </body>
         </Html>
     )
